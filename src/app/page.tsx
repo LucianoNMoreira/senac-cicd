bde49c008171064ba0e58decaaaf0e1ea175b6d8
Luciano Nobre Moreira <lucianonobremoreira@gmail.com>
2025-04-15 19:55:23 -0300

    quebrar app para aula

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 
-const SUBPATH = process.env.SUBPATH || '';
+// const SUBPATH = process.env.SUBPATH || '';
+const SUBPATH = '';
 
 export default function Home() {
   return (
