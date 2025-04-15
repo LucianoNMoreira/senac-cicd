3a680f3fd92e6a7afe9b18509742a5e511b5b978
Luciano Nobre Moreira <lucianonobremoreira@gmail.com>
2025-04-15 08:47:29 -0300

    corrigir SUBPATH das imagens

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // https://nextjs.org/docs/app/api-reference/config/next-config-js/output
   output: 'export',
+  // https://nextjs.org/docs/app/api-reference/config/next-config-js/distDir
   // distDir: 'dist',
+
+  // https://nextjs.org/docs/app/api-reference/config/next-config-js/basePath
   basePath: process.env.SUBPATH || '/',
+  // https://nextjs.org/docs/app/api-reference/config/next-config-js/assetPrefix
   assetPrefix: process.env.SUBPATH || '/',
 };
 
