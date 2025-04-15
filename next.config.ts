8807ea6e9c1511830e59ad876fdf228775464eaa
Luciano Nobre Moreira <lucianonobremoreira@gmail.com>
2025-04-15 08:40:37 -0300

    deploy com SUBPATH

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
-  // distDir: 'dist'
+  // distDir: 'dist',
+  basePath: process.env.SUBPATH || '/',
+  assetPrefix: process.env.SUBPATH || '/',
 };
 
 export default nextConfig;
